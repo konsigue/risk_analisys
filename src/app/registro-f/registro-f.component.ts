5933a028f1be6230221db59d33f031ba3c9c1484
Gerardo Arjona <gerardo.arjona@konsigue.com>
2018-06-15 17:18:01 -0500

    Se agregaron animaciones fadeIn a las pantallas
    Signed-off-by: Gerardo Arjona <gerardo.arjona@konsigue.com>

--- src/app/registro-f/registro-f.component.ts
+++ src/app/registro-f/registro-f.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  animate,
+  style
+} from '@angular/animations';
 
 @Component({
   selector: 'app-registro-f',
   templateUrl: './registro-f.component.html',
-  styleUrls: ['./registro-f.component.css']
+  styleUrls: ['./registro-f.component.css'],
+  animations: [
+    trigger('animationSwitch', [
+      transition('void=>*', [
+        style({opacity:0}),
+        animate(2000),
+      ])
+    ])
+  ]
 })
 export class RegistroFComponent implements OnInit {
 
