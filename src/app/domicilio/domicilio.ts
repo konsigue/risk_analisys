36b95d1349657c2ee1a44d36dc44be5573628769
Martín <martin.lopez@konsigue.com>
2018-06-19 12:02:17 -0500

    Add Domicilio interface model

---
+++ src/app/domicilio/domicilio.ts
@@ -0,0 +1,9 @@
+export interface Domicilio {
+    ZipCode: number,
+    State: string,
+    Township: string,
+    City: string,
+    District: string,
+    ExteriorNumber: number,
+    InteriorNumber: number
+}
