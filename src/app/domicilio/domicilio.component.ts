ea5cce83dbceac8ff739cd22771bcdf00b1ee192
Martín <martin.lopez@konsigue.com>
2018-06-19 12:44:12 -0500

    Add search button after zipcode for getting address data

--- src/app/domicilio/domicilio.component.ts
+++ src/app/domicilio/domicilio.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DomicilioService } from './domicilio.service';
+import { Domicilio } from './domicilio';
 
 @Component({
   selector: 'app-domicilio',
   templateUrl: './domicilio.component.html',
-  styleUrls: ['./domicilio.component.css']
+  styleUrls: ['./domicilio.component.css'],
+  providers: [DomicilioService]
 })
 export class DomicilioComponent implements OnInit {
 
-  constructor() { }
+  address: Domicilio;
+
+  constructor(private domicilioService: DomicilioService) { }
 
   ngOnInit() {
   }
