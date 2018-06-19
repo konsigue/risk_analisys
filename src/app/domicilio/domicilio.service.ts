ec4491593b480c07b2851e8bb99f5d167b05563d
Martín <martin.lopez@konsigue.com>
2018-06-19 12:15:18 -0500

    Add getAddress GET method on DomicilioService

---
+++ src/app/domicilio/domicilio.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Domicilio } from './domicilio';
+
+@Injectable()
+export class DomicilioService {
+    zipCodeUrl = "https://api-codigos-postales.herokuapp.com/v2/codigo_postal/"
+    
+    constructor(private http: HttpClient) {
+
+    }
+
+    /**
+     * GET Address associated with the zip code and a list of districts from that zip code
+     * @param zipCode number of the user given zipCode
+     */
+    getAddress(zipCode: number): Observable<Domicilio> {
+        return this.http.get<Domicilio>(this.zipCodeUrl + zipCode);
+    }
+}
