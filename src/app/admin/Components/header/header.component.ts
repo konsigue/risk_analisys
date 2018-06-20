317afb58583cd35161c7d5a094e27c02b58df3bd
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-06-20 14:02:13 -0500

    Admin header
    This header is only for admin users

---
+++ src/app/admin/Components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
