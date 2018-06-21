eb3d8afa8f3eacec4c29c8455065bd47d931100d
Gerardo Arjona <gerardo.arjona@konsigue.com>
2018-06-21 18:15:27 -0500

    Se termino la pantalla de registrar accionistas y se corrigio el bug visual de tarjeta
    Signed-off-by: Gerardo Arjona <gerardo.arjona@konsigue.com>

--- src/app/tarjeta/tarjeta.component.ts
+++ src/app/tarjeta/tarjeta.component.ts
@@ -1,16 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  animate,
+  style,
+  state
+} from '@angular/animations';
 
 @Component({
   selector: 'app-targeta',
   templateUrl: './tarjeta.component.html',
-  styleUrls: ['./tarjeta.component.css']
+  styleUrls: ['./tarjeta.component.css'],
+  animations: [
+    trigger('clicked', [
+      state('inactive', style({
+        backgroundColor: '#495057',
+        transform: 'scale(1)'
+      })),
+      state('active',   style({
+        backgroundColor: '#aac111',
+        color: '#000000',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ]),
+    trigger('clicked2', [
+      state('active', style({
+        backgroundColor: '#495057',
+        transform: 'scale(1)'
+      })),
+      state('inactive',   style({
+        backgroundColor: '#aac111',
+        color: '#000000',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ],
+  
 })
 export class TarjetaComponent implements OnInit {
 
-  mostrar = true;
 
 
   constructor() { }
+
+  state='inactive';
+  mostrar= false
+
+  toggleState(n:number) {
+    if(n==1){
+      this.state = 'active';
+      this.mostrar=true;
+    }
+    if(n==0){
+      this.state = 'inactive';
+      this.mostrar=false;
+    }
+
+  }
 
   ngOnInit() {
   }
