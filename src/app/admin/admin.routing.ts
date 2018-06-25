c93c90b205282872461a2ec3045e4579fbcf9027
Martín <martin.lopez@konsigue.com>
2018-06-25 16:23:25 -0500

    Add supplier-analysis on router

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminInicioComponent } from './Components/admin-inicio/admin-inicio.component';
 import { adminComponent } from './admin.component';
 import { ProvidersComponent } from './Components/providers/providers.component';
+import { SupplierAnalysisComponent } from './Components/supplier-analysis/supplier-analysis.component';
 
 const routes: Routes = [
   {
@@ -15,11 +16,16 @@ const routes: Routes = [
       { path: '', outlet: 'admin', component: AdminInicioComponent, pathMatch: 'full' }
     ]
   },
- {
-   path: 'proveedores', component: adminComponent, children: [
-     { path: '', outlet: 'admin', component: ProvidersComponent, pathMatch: 'full' },
-   ]
- }
+  {
+    path: 'proveedores', component: adminComponent, children: [
+      { path: '', outlet: 'admin', component: ProvidersComponent, pathMatch: 'full' },
+    ]
+  },
+  {
+    path: 'proveedores/:id/analisis', component: adminComponent, children: [
+      { path: '', outlet: 'admin', component: SupplierAnalysisComponent, pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
