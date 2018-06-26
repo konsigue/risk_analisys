b73513c8cb6e35ed5f706347866288298b14c7d1
Martín <martin.lopez@konsigue.com>
2018-06-26 17:55:38 -0500

    Add FinancialSituation model

---
+++ src/app/admin/supplier/models/FinancialSituationModel.ts
@@ -0,0 +1,8 @@
+export class FinancialSituationModel {
+    public InmediateLiquidity: number
+    public MediateLiquidity: number
+    public FinancialAppeceament: number
+    public TotalActives: number
+    public TotalSales: number
+    public Uafirda: number
+}
