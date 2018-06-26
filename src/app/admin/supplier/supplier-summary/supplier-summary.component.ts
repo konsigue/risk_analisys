58bc2a7c6c002e751e5ae6caf3c3c048fac314b8
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-06-26 18:53:16 -0500

    Style: logo fix, Fix suppliet summary unused elements

--- src/app/admin/supplier/supplier-summary/supplier-summary.component.ts
+++ src/app/admin/supplier/supplier-summary/supplier-summary.component.ts
@@ -7,9 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupplierSummaryComponent implements OnInit {
 
-  provider_name = 'Soluciones Digitales Rubio Haro y asociados.';
-  provider_description = 'Empresa dedicada a la consultoria sobre informatica, redes, software y tecnologia en general para empresas y entornos empresariales.';
-
   constructor() { }
 
   ngOnInit() {
