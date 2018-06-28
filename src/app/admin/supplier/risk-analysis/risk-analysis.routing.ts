d715402e8a259a8dbc1b98038ff03201268649c3
Martín <martin.lopez@konsigue.com>
2018-06-28 16:38:15 -0500

    Add AnalysisFinancialStatus component with router-outlet

--- src/app/admin/supplier/risk-analysis/risk-analysis.routing.ts
+++ src/app/admin/supplier/risk-analysis/risk-analysis.routing.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SupplierAnalysisComponent } from './supplier-analysis.component';
+import { AnalysisFinancialStatusComponent } from './analysis-financial-status/analysis-financial-status.component';
+import { AdminComponent } from '../../admin.component';
 
 const routes: Routes = [
   {
-    path: 'admin/proveedores/:name/analisis/status', component: SupplierAnalysisComponent, children: [
-      { path: '', outlet: 'risk-analysis', component: SupplierAnalysisComponent, pathMatch: 'full' }
+    path: 'admin/proveedores/:name/analisis/estatus', component: AdminComponent, children: [
+      { 
+        path: '', outlet: 'admin', component: SupplierAnalysisComponent, children: [
+          { path: '', outlet: 'risk-analysis', component: AnalysisFinancialStatusComponent }
+        ]
+      }
     ]
   }
 ];
--- src/app/admin/supplier/risk-analysis/supplier-analysis.component.ts
+++ src/app/admin/supplier/risk-analysis/supplier-analysis.component.ts
@@ -8,8 +8,6 @@ import {
 } from '@angular/animations';
 
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { FiscalStatusModel } from '../models/FiscalStatusModel';
-import { FinancialSituationModel } from '../models/FinancialSituationModel';
 
 @Component({
   selector: 'app-supplier-analysis',
@@ -34,35 +32,9 @@ import { FinancialSituationModel } from '../models/FinancialSituationModel';
 })
 export class SupplierAnalysisComponent implements OnInit {
   public supplierName: string
-  public fiscalStatus: FiscalStatusModel
-  public financialSituation: FinancialSituationModel[]
 
   constructor(private router: Router, private route: ActivatedRoute) {
-    this.fiscalStatus = new FiscalStatusModel()
-    this.financialSituation = new Array(4)
 
-    this.financialSituation = [
-      { 
-        Name: "2016", InmediateLiquidity: "1.88", MediateLiquidity: "2.07", 
-        FinancialAppeceament: "1.05", TotalActives: "29419", TotalSales: "49110",
-        Uafirda: "-762"
-      },
-      {
-        Name: "2017", InmediateLiquidity: "2.34", MediateLiquidity: "2.48", 
-        FinancialAppeceament: "0.75", TotalActives: "26714", TotalSales: "60263",
-        Uafirda: "2222"
-      },
-      {
-        Name: "Variación", InmediateLiquidity: "0.46", MediateLiquidity: "0.41", 
-        FinancialAppeceament: "-0.30", TotalActives: "-9.19%", TotalSales: "22.71%",
-        Uafirda: "391.60%" 
-      },
-      {
-        Name: "Tendencia", InmediateLiquidity: "estable", MediateLiquidity: "estable", 
-        FinancialAppeceament: "estable", TotalActives: "negativa", TotalSales: "positiva",
-        Uafirda: "positiva" 
-      },
-    ]
   }
 
   ngOnInit() {
@@ -72,7 +44,7 @@ export class SupplierAnalysisComponent implements OnInit {
   }
 
   onNavChange(e: string) {
-    this.router.navigate(['admin/proveedores/' + this.supplierName + '/' + e], { replaceUrl : true });
+    this.router.navigate(['admin/proveedores/' + this.supplierName + '/analisis/' + e], { replaceUrl : true });
   }
 
 }
