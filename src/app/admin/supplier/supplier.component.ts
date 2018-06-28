5d65fdc36c0ec9eb45f642a0b25c8a8e16150da6
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-06-28 17:53:48 -0500

    add general info data and supplier model

--- src/app/admin/supplier/supplier.component.ts
+++ src/app/admin/supplier/supplier.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Supplier } from './supplier';
+import { DomicilioComponent } from '../../register/domicilio/domicilio.component';
+import { Domicilio } from '../../register/domicilio/domicilio';
 
 @Component({
   selector: 'app-supplier',
@@ -6,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supplier.component.css']
 })
 export class SupplierComponent implements OnInit {
+  supplier;
+  adress = {
+    codigo_postal: 7020,
+    estado: 'CDMX',
+    municipio: 'GAM',
+    ciudad: 'CDMX',
+    colonias: ['Tepeyac Insurgentes'],
+    numero_exterior: 12,
+    numero_interior: null,
+  };
+
+  getAdress() {
+    return <Domicilio>(this.adress);
+  }
+
+  getSupplier() {
+    // tslint:disable-next-line:max-line-length
+    return new Supplier(1, 'RHD990710RUB', 'Rubio Haro Digital', 'Servicios Digitales Rubio Haro S.A. de C.V.', 'Empresa que brinda servicios y soluciones digitales con un enfoque de crecimiento para las pymes', 'CDMX', 'Tecnológica', 'Consultorias digitales', this.getAdress());
+  }
 
 
-  constructor() { }
+  constructor() {
+    this.supplier = this.getSupplier();
+  }
 
   ngOnInit() {
   }
