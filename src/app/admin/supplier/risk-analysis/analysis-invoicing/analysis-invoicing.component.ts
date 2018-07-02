1d55af1dfa13c3f4e89223279b25fef4c59e5761
Martín <martin.lopez@konsigue.com>
2018-07-02 11:38:17 -0500

    Fix object model initialization

--- src/app/admin/supplier/risk-analysis/analysis-invoicing/analysis-invoicing.component.ts
+++ src/app/admin/supplier/risk-analysis/analysis-invoicing/analysis-invoicing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoicingModel, InvoiceDataModel } from '../models/InvoicingModel';
 
 @Component({
   selector: 'analysis-invoicing',
@@ -7,40 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalysisInvoicingComponent implements OnInit {
 
-  constructor() { }
+  public data: InvoiceDataModel[]
+  public invoiceModel: InvoicingModel
 
   // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    }
-  ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
+  public lineChartData:Array<any>;
+  public lineChartLabels:Array<any>;
+  public lineChartOptions:any
+  public lineChartColors:Array<any>
+  public lineChartLegend:boolean;
+  public lineChartType:string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.data = new Array<InvoiceDataModel>()
+    this.invoiceModel = new InvoicingModel()
 
+    // lineChart
+    this.lineChartData = this.invoiceModel.ChartData;
+    this.lineChartLabels = this.invoiceModel.ChartLabels;
+    this.lineChartOptions = {
+      responsive: true
+    };
+    this.lineChartColors = [
+      { // grey
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      },
+      { // dark grey
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: 'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(77,83,96,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(77,83,96,1)'
+      }
+    ];
+    this.lineChartLegend = true;
+    this.lineChartType = 'line';
   }
 
   public randomize():void {
