276679a881f47f4586e8a744f3bb21ddd51451b9
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-07-02 12:06:54 -0500

    fix: beautify code to supplier-general-info component

--- src/app/admin/supplier/supplier-general-info/supplier-general-info.component.ts
+++ src/app/admin/supplier/supplier-general-info/supplier-general-info.component.ts
@@ -7,29 +7,28 @@ import { SupplierComponent } from '../supplier.component';
   styleUrls: ['./supplier-general-info.component.css']
 })
 export class SupplierGeneralInfoComponent implements OnInit {
-  public toggle: boolean
-  public arrow: string
+  public toggle: boolean;
+  public arrow: string;
 
-  constructor() { 
-    this.toggle = true
+  constructor() {
+    this.toggle = true;
   }
 
   ngOnInit() {
-    this.arrow = "fa-chevron-down"
+    this.arrow = 'fa-chevron-down';
   }
 
   toggleArrow() {
-    this.toggle = !this.toggle
-    this.arrow = this.getArrowClass()
+    this.toggle = !this.toggle;
+    this.arrow = this.getArrowClass();
   }
 
   getArrowClass() {
-    console.log(this.toggle)
+    console.log(this.toggle);
     if (this.toggle) {
-      return "fa-chevron-down"
-    }
-    else {
-      return "fa-chevron-right"
+      return 'fa-chevron-down';
+    } else {
+      return 'fa-chevron-right';
     }
   }
 }
