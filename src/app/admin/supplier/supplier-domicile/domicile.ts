d8ad7e9b44223fbdad32b6282a0866e4fcc74c17
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-07-03 11:46:16 -0500

    add new Domicile model

---
+++ src/app/admin/supplier/supplier-domicile/domicile.ts
@@ -0,0 +1,13 @@
+
+export interface Domicile {
+  street: string;
+  street_number: string;
+  adress_details: string;
+  town: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  country: string;
+  zone: string;
+}
+
