7693a871f1a8b6bf0d4a822ce70e4b42daab2504
Martín <martin.lopez@konsigue.com>
2018-07-03 12:57:24 -0500

    Fix table headers

--- src/app/admin/supplier/risk-analysis/analysis-financial/analysis-financial.component.ts
+++ src/app/admin/supplier/risk-analysis/analysis-financial/analysis-financial.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FinancialAlgorithmModel } from '../models/FinancialAlgorithmModel';
+import { FinancialAnalysisModel } from '../models/FinancialAnalysisModel';
 
 @Component({
   selector: 'analysis-financial',
@@ -8,16 +9,23 @@ import { FinancialAlgorithmModel } from '../models/FinancialAlgorithmModel';
 })
 export class AnalysisFinancialComponent implements OnInit {
   public financialAlgorithm: FinancialAlgorithmModel[]
+  public financialAnalysis: FinancialAnalysisModel[]
 
   constructor() { }
 
   ngOnInit() {
     this.financialAlgorithm = []
+    this.financialAnalysis = []
 
     /** mock */
     this.financialAlgorithm = [
       { Time: "2017", Value: 7.36 },
       { Time: "Últimos 12 meses", Value: 11.55 }
+    ]
+    this.financialAnalysis = [
+      {Time:"2016",CashFlow:-752,AccountingCapital:14320,NetSales:49110,TotalAssets:29419},
+      {Time:"2017",CashFlow:2424,AccountingCapital:15268,NetSales:60263,TotalAssets:26714},
+      {Time:"Últimos 12 meses",CashFlow:18737,AccountingCapital:19184,NetSales:82147,TotalAssets:30630}
     ]
     /** end mock */
   }
