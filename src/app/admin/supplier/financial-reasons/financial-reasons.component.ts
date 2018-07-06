964a425eb9fc225263d98481b2531c1de91d21f6
Mike <mforti1@yahoo.com.mx>
2018-07-06 16:45:29 -0500

    En las pantallas de estados financieros y razones financieras se hicieron datos de prueba

--- src/app/admin/supplier/financial-reasons/financial-reasons.component.ts
+++ src/app/admin/supplier/financial-reasons/financial-reasons.component.ts
@@ -9,7 +9,88 @@ export class FinancialReasonsComponent implements OnInit {
 
   constructor() { }
 
+    razonesFinancieras = [
+
+    { 'Menu': 'Parcial hasta', 'Anio': '2009', 'CrecimientoNominal': '8',
+      'MargenOperativo': '6', 'Productividad': '2', 'MargenNeto': '5', 'RazonCirculante': '4',
+      'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 'DiasProveedores': '3',
+      'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9', 'RazonCapTrab': '3'},
+
+     {'Menu': 'Ejercicio 2010', 'Anio': '2010', 'CrecimientoNominal': '7',
+      'MargenOperativo': '6', 'Productividad': '2', 'MargenNeto': '5',
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+      'RazonCapTrab': '3'},
+
+    {'Menu': 'Ejercicio 2011', 'Anio': '2011', 'CrecimientoNominal': '6',
+     'MargenOperativo': '6', 'Productividad': '2', 'MargenNeto': '5', 
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+
+    {'Menu': 'Ejercicio 2012', 'Anio': '2012', 'CrecimientoNominal': '5', 
+    'MargenOperativo': '2', 'Productividad': '3', 'MargenNeto': '4', 
+    'RazonCirculante': '5', 'PruebaDelAcido': '5', 'PasivoTotal': '4', 'ActivoTotal': '8', 
+    'DiasProveedores': '6', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+
+    {'Menu': 'Ejercicio 2013', 'Anio': '2013', 'CrecimientoNominal': '4', 
+    'MargenOperativo': '6', 'Productividad': '2016', 'MargenNeto': '5', 
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+  
+    {'Menu': 'Ejercicio 2014', 'Anio': '2014', 'CrecimientoNominal': '3',
+     'MargenOperativo': '6', 'Productividad': '2016', 'MargenNeto': '5', 
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+  
+    {'Menu': 'Ejercicio 2015', 'Anio': '2015', 'CrecimientoNominal': '2',
+     'MargenOperativo': '6', 'Productividad': '2016', 'MargenNeto': '5', 
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+  
+    {'Menu': 'Ejercicio 2016', 'Anio': '2016', 'CrecimientoNominal': '1',
+     'MargenOperativo': '6', 'Productividad': '2016', 'MargenNeto': '5', 
+    'RazonCirculante': '4', 'PruebaDelAcido': '2', 'PasivoTotal': '3', 'ActivoTotal': '6', 
+    'DiasProveedores': '3', 'DiasInventarios': '7', 'CicloFinanciero': '9', 'CapitalDeTrabajo': '9',
+    'RazonCapTrab': '3'},
+    ];
+
+  
+    
+
   ngOnInit() {
+    //  let arreglo = [];
+    
+    
+
+    // let desde = 2010;
+    // let hasta = desde + 6;
+    // let v = 2010;
+    
+    // for (let i = 0; i <= 6; i++) {
+    //     arreglo[i] = v++;
+    //     console.log(i);
+    // }
+
+    // for (let i = 0; i < info.length; i++) {
+      
+    // }
+  
+
+
+  }
+
+  alerta():void{
+  console.log("Hola");
+  }
+  
+  findYear():void{
+    let from = document.getElementById("select-from")
   }
 
 }
+
--- src/app/admin/supplier/financial-statements/financial-statements.component.ts
+++ src/app/admin/supplier/financial-statements/financial-statements.component.ts
@@ -5,11 +5,59 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './financial-statements.component.html',
   styleUrls: ['./financial-statements.component.css']
 })
+
+
 export class FinancialStatementsComponent implements OnInit {
 
   constructor() { }
+
+  estadosFinancieros = [
+
+    { 'Menu': 'Parcial hasta', 'VentasNetas': '8',
+      'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5', 'Depreciacion': '4',
+      'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 'UtilidadAntesImpuestos': '3',
+      'Impuestos': '7'},
+
+     {'Menu': 'Ejercicio 2010', 'Anio': '2010', 'VentasNetas': '7',
+      'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5',
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+
+    {'Menu': 'Ejercicio 2011', 'Anio': '2011', 'VentasNetas': '6',
+     'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5', 
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+
+    {'Menu': 'Ejercicio 2012', 'Anio': '2012', 'VentasNetas': '5', 
+    'CostoVentas': '2', 'UtilidadVentas': '3', 'GastoAdministracion': '4', 
+    'Depreciacion': '5', 'UtilidadOperacion': '5', 'GastosFinancieros': '4', 'ProductosFinancieros': '8',
+    'UtilidadAntesImpuestos': '6', 'Impuestos': '7'},
+
+    {'Menu': 'Ejercicio 2013', 'Anio': '2013', 'VentasNetas': '4', 
+    'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+  
+    {'Menu': 'Ejercicio 2014', 'Anio': '2014', 'VentasNetas': '3',
+     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+  
+    {'Menu': 'Ejercicio 2015', 'Anio': '2015', 'VentasNetas': '2',
+     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+  
+    {'Menu': 'Ejercicio 2016', 'Anio': '2016', 'VentasNetas': '1',
+     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
+    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
+    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+    ];
+
+  
 
   ngOnInit() {
   }
 
 }
+
