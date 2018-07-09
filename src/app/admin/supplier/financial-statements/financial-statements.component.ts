b77f8695349f16372fff30cdd5695940af3b95d9
Rodrigo (Roy) <chavo0022009@gmail.com>
2018-07-09 09:59:52 -0500

    fix: merge conflicts

--- src/app/admin/supplier/financial-statements/financial-statements.component.ts
+++ src/app/admin/supplier/financial-statements/financial-statements.component.ts
@@ -10,56 +10,66 @@ import { Component, OnInit } from '@angular/core';
 export class FinancialStatementsComponent implements OnInit {
 
   constructor() { }
-<<<<<<< HEAD
 
   estadosFinancieros = [
 
-    { 'Menu': 'Parcial hasta', 'VentasNetas': '8',
+    {
+      'Menu': 'Parcial hasta', 'VentasNetas': '8',
       'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5', 'Depreciacion': '4',
       'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 'UtilidadAntesImpuestos': '3',
-      'Impuestos': '7'},
+      'Impuestos': '7'
+    },
 
-     {'Menu': 'Ejercicio 2010', 'Anio': '2010', 'VentasNetas': '7',
+    {
+      'Menu': 'Ejercicio 2010', 'Anio': '2010', 'VentasNetas': '7',
       'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5',
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
 
-    {'Menu': 'Ejercicio 2011', 'Anio': '2011', 'VentasNetas': '6',
-     'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5', 
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
+    {
+      'Menu': 'Ejercicio 2011', 'Anio': '2011', 'VentasNetas': '6',
+      'CostoVentas': '6', 'UtilidadVentas': '2', 'GastoAdministracion': '5',
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
 
-    {'Menu': 'Ejercicio 2012', 'Anio': '2012', 'VentasNetas': '5', 
-    'CostoVentas': '2', 'UtilidadVentas': '3', 'GastoAdministracion': '4', 
-    'Depreciacion': '5', 'UtilidadOperacion': '5', 'GastosFinancieros': '4', 'ProductosFinancieros': '8',
-    'UtilidadAntesImpuestos': '6', 'Impuestos': '7'},
+    {
+      'Menu': 'Ejercicio 2012', 'Anio': '2012', 'VentasNetas': '5',
+      'CostoVentas': '2', 'UtilidadVentas': '3', 'GastoAdministracion': '4',
+      'Depreciacion': '5', 'UtilidadOperacion': '5', 'GastosFinancieros': '4', 'ProductosFinancieros': '8',
+      'UtilidadAntesImpuestos': '6', 'Impuestos': '7'
+    },
 
-    {'Menu': 'Ejercicio 2013', 'Anio': '2013', 'VentasNetas': '4', 
-    'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
-  
-    {'Menu': 'Ejercicio 2014', 'Anio': '2014', 'VentasNetas': '3',
-     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
-  
-    {'Menu': 'Ejercicio 2015', 'Anio': '2015', 'VentasNetas': '2',
-     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
-  
-    {'Menu': 'Ejercicio 2016', 'Anio': '2016', 'VentasNetas': '1',
-     'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5', 
-    'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6', 
-    'UtilidadAntesImpuestos': '3', 'Impuestos': '7'},
-    ];
+    {
+      'Menu': 'Ejercicio 2013', 'Anio': '2013', 'VentasNetas': '4',
+      'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5',
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
 
-  
+    {
+      'Menu': 'Ejercicio 2014', 'Anio': '2014', 'VentasNetas': '3',
+      'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5',
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
 
-=======
-  
->>>>>>> 70ab938a25345fd62cecca8198f0933965aeae6d
+    {
+      'Menu': 'Ejercicio 2015', 'Anio': '2015', 'VentasNetas': '2',
+      'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5',
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
+
+    {
+      'Menu': 'Ejercicio 2016', 'Anio': '2016', 'VentasNetas': '1',
+      'CostoVentas': '6', 'UtilidadVentas': '2016', 'GastoAdministracion': '5',
+      'Depreciacion': '4', 'UtilidadOperacion': '2', 'GastosFinancieros': '3', 'ProductosFinancieros': '6',
+      'UtilidadAntesImpuestos': '3', 'Impuestos': '7'
+    },
+  ];
+
   ngOnInit() {
   }
 
